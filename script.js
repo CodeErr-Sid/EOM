d138554c80c602acd86ca716646060c5d376a2e6
primesiteinnovation <primesiteinnovation@gmail.com>
2024-05-27 14:45:38 +0530

    about section

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ document.addEventListener('DOMContentLoaded', () => {
         menuIcon.classList.toggle('change');
     });
 });
+
+// About Section
+document.getElementById('image-stack').addEventListener('click', function() {
+    const topImage = document.getElementById('image1');
+    const bottomImage = document.getElementById('image2');
+
+    if (topImage.classList.contains('image-top')) {
+        topImage.classList.remove('image-top');
+        topImage.classList.add('image-bottom');
+        bottomImage.classList.remove('image-bottom');
+        bottomImage.classList.add('image-top');
+    } else {
+        topImage.classList.remove('image-bottom');
+        topImage.classList.add('image-top');
+        bottomImage.classList.remove('image-top');
+        bottomImage.classList.add('image-bottom');
+    }
+});
