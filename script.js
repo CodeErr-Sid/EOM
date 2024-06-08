ecbcbee46a0066bd3be9c416cc9e45cae4c4e3be
primesiteinnovation <primesiteinnovation@gmail.com>
2024-06-08 17:39:21 +0530

    fixes sid loom

--- script.js
+++ script.js
@@ -483,39 +483,104 @@ window.onload = function () {
 
 // gallery
 
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 
-var swiper = new Swiper(`[unique-script-id="w-w-dm-id"] .mySwiper`, {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: `[unique-script-id="w-w-dm-id"] .swiper-pagination`,
-        clickable: true,
-    },
-    breakpoints: {
-        200: {
-            slidesPerView: 1
-        },
-        501: {
-            slidesPerView: 1.5
-        },
-        769: {
-            slidesPerView: 2.5,
-            spaceBetween: 10
-        },
-        1025: {
-            slidesPerView: 3,
-            spaceBetween: 20
-        },
-    },
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: `[unique-script-id="w-w-dm-id"] .swiper-button-next`,
-        prevEl: `[unique-script-id="w-w-dm-id"] .swiper-button-prev`,
-    },
-    loop: true,
+// var swiper = new Swiper(`[unique-script-id="w-w-dm-id"] .mySwiper`, {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     pagination: {
+//         el: `[unique-script-id="w-w-dm-id"] .swiper-pagination`,
+//         clickable: true,
+//     },
+//     breakpoints: {
+//         200: {
+//             slidesPerView: 1
+//         },
+//         501: {
+//             slidesPerView: 1.5
+//         },
+//         769: {
+//             slidesPerView: 2.5,
+//             spaceBetween: 10
+//         },
+//         1025: {
+//             slidesPerView: 3,
+//             spaceBetween: 20
+//         },
+//     },
+//     autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//     },
+//     navigation: {
+//         nextEl: `[unique-script-id="w-w-dm-id"] .swiper-button-next`,
+//         prevEl: `[unique-script-id="w-w-dm-id"] .swiper-button-prev`,
+//     },
+//     loop: true,
+// });
+
+
+// faq
+
+let question = document.querySelectorAll(".question");
+
+question.forEach(question => {
+  question.addEventListener("click", event => {
+    const active = document.querySelector(".question.active");
+    if(active && active !== question ) {
+      active.classList.toggle("active");
+      active.nextElementSibling.style.maxHeight = 0;
+    }
+    question.classList.toggle("active");
+    const answer = question.nextElementSibling;
+    if(question.classList.contains("active")){
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = 0;
+    }
+  })
+})
+
+// typing
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["content1", "content2", "content3", "content4"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
